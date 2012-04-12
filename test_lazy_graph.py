bf04816de6406dc71212658ebc3ead46a6cb6f8f
Vaibhav Mallya <vaibhavmallya@gmail.com>
2012-04-12 02:04:04 -0700

    Adding test to make sure that default values at the origin only include valid values.

--- test_lazy_graph.py
+++ test_lazy_graph.py
@@ -10,12 +10,22 @@ class TestLazyGraph(TestShortestPaths):
     def testDefaultCentralRetrievalReturnsUnitEdgeWeights(self):
         coord = Coord(5,5)
         # All adjacent to (5,5). No self-edge.
-        desired = [WeightedEndOfEdge(1, Coord(5,6)),
-                         WeightedEndOfEdge(1, Coord(5,4)),
-                         WeightedEndOfEdge(1, Coord(4,5)),
-                         WeightedEndOfEdge(1, Coord(6,5))]
+        desired = [ WeightedEndOfEdge(1, Coord(5,6)),
+                    WeightedEndOfEdge(1, Coord(5,4)),
+                    WeightedEndOfEdge(1, Coord(4,5)),
+                    WeightedEndOfEdge(1, Coord(6,5))]
         actual = self.gridonly_lazy_graph[coord]
         self.assertEqual(
             set(desired),
             set(actual))
 
+    def testDefaultOriginRetrievalReturnsTwoUnitEdgeWeights(self):
+        coord = Coord(0,0)
+        desired = [ WeightedEndOfEdge(1, Coord(0,1)),
+                    WeightedEndOfEdge(1, Coord(1,0)) ]
+        actual = self.gridonly_lazy_graph[coord]
+
+        self.assertEqual(
+            set(desired),
+            set(actual))
+    
