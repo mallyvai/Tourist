dc7aacf42a7874f894c674b6c68063cc24184f8d
Vaibhav Mallya <vaibhavmallya@gmail.com>
2012-04-12 01:51:27 -0700

    tests for the lazy graph object

---
+++ test_lazy_graph.py
@@ -0,0 +1,21 @@
+import unittest
+from shared_objects import LazyGraph, Coord, WeightedEndOfEdge
+from test_shortest_paths import TestShortestPaths
+
+class TestLazyGraph(TestShortestPaths):
+
+    def setUp(self):
+        self.gridonly_lazy_graph = LazyGraph(10, [], [])
+
+    def testDefaultCentralRetrievalReturnsUnitEdgeWeights(self):
+        coord = Coord(5,5)
+        # All adjacent to (5,5). No self-edge.
+        desired = [WeightedEndOfEdge(1, Coord(5,6)),
+                         WeightedEndOfEdge(1, Coord(5,4)),
+                         WeightedEndOfEdge(1, Coord(4,5)),
+                         WeightedEndOfEdge(1, Coord(6,5))]
+        actual = self.gridonly_lazy_graph[coord]
+        self.assertEqual(
+            set(desired),
+            coord)
+
